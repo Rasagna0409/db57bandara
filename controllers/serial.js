3b8f1b1beb090f8ec1107e3edf45ad24315afaf6
Rasagna0409 <77762529+Rasagna0409@users.noreply.github.com>
2021-04-14 21:12:40 -0500

    Added update delete create

--- controllers/serial.js
+++ controllers/serial.js
@@ -11,9 +11,16 @@ exports.serial_list = async function (req, res) {
 };
 
 // for a specific serial.
-exports.serial_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: serial detail: ' + req.params.id);
-};
+exports.serial_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await serial.findById( req.params.id)
+    res.send(result)
+     } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+     }
+    };
 // Handle serial create on POST.
 exports.serial_create_post = async function (req, res) {
     console.log(req.body)
@@ -28,28 +35,106 @@ exports.serial_create_post = async function (req, res) {
     try {
         let result = await document.save();
         res.send(result);
-    } catch (err) {
-        res.error(500, `{"error": ${err}}`);
+    }
+    catch (err) {
+        res.send(`{"error": ${err}}`)
+        res.status(500);
     }
 };
 // Handle serial delete form on DELETE.
-exports.serial_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: serial delete DELETE ' + req.params.id);
+exports.serial_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await serial.findByIdAndDelete( req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
-// Handle serial update form on PUT.
-exports.serial_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: serial update PUT' + req.params.id);
+//Handle serial update form on PUT.
+exports.serial_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+try {
+    let toUpdate = await serial.findById( req.params.id)
+// Do updates of properties
+    if(req.body.name) toUpdate.name = req.body.name;
+    if(req.body.author) toUpdate.author = req.body.author;
+    if(req.body.season) toUpdate.season = req.body.season;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } 
+    catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
+
 // VIEWS
 // Handle a show all view
 exports.serial_view_all_Page = async function (req, res) {
     try {
         theserials = await serial.find();
-        res.render('serials', {
-            title: 'serial Search Results',
-            results: theserials
-        });
-    } catch (err) {
-        res.error(500, `{"error": ${err}}`);
+        res.render('serials', { title: 'serial Search Results', results: theserials });
+    }
+    catch (err) {
+        res.send(`{"error": ${err}}`)
+        res.status(500);
+    }
+};
+
+// Handle a show one view with id specified by query
+exports.serial_view_one_Page = async function(req, res) {
+    console.log("single view for id "  + req.query.id)
+    try{
+        result = await serial.findById( req.query.id)
+        res.render('serialdetail', { title: 'Serial Detail', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.serial_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+        res.render('serialcreate', { title: 'Serial Create'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for updating a fish.
+// query provides the id
+exports.serial_update_Page =  async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await serial.findById(req.query.id)
+        res.render('serialupdate', { title: 'Serial Update', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.serial_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await serial.findById(req.query.id)
+        res.render('serialdelete', { title: 'Serial Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
     }
 };
--- routes/serials.js
+++ routes/serials.js
@@ -1,6 +1,18 @@
 var express = require('express');
 const serial_controlers= require('../controllers/serial');
 var router = express.Router();
-/* GET serials */
+/* GET serial */
 router.get('/', serial_controlers.serial_view_all_Page );
 module.exports = router; 
+
+/* GET detail serial page */
+router.get('/detail', serial_controlers.serial_view_one_Page);
+
+/* GET create serial page */
+router.get('/create', serial_controlers.serial_create_Page);
+
+/* GET create update page */
+router.get('/update', serial_controlers.serial_update_Page);
+
+/* GET create Icecream page */
+router.get('/delete', serial_controlers.serial_delete_Page);
